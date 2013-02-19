f503960acf18633b5d0219110ff4918dcd296cc5
Uwe Stuehler <uwe@bsdx.de>
2013-02-19 22:05:45 +0100

    More nodes added to the site

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,29 @@
 $bacula_use_file_concat = true
 
-node 'p1.puppetize.net' {
+node default
+{
   include site::admin_users
   include site::bacula
+}
+
+node 'p1.puppetize.net' inherits default
+{
   include site::openstack::all
 }
 
-node 'www.puppetize.net' {
+node 'p2.puppetize.net' inherits default
+{
+  include site::openstack::all
+}
+
+node 'puppet.puppetize.net' inherits default
+{
+  # TODO
+  include site::vim::puppet
+}
+
+node 'www.puppetize.net' inherits default
+{
+  # TODO
   include apache
 }
